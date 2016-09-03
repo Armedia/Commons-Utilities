636c8112aea04170c65ec899d64eb56892e2d502
Diego Rivera <diego.rivera@armedia.com>
2016-09-03 16:04:57 +0000

    Updated to the new CloseableIterator model which more closely resembles traditional iterators.

--- src/main/java/com/armedia/commons/utilities/CloseableIterator.java
+++ src/main/java/com/armedia/commons/utilities/CloseableIterator.java
@@ -6,20 +6,31 @@ import java.util.NoSuchElementException;
 
 public abstract class CloseableIterator<E> implements Closeable, Iterator<E> {
 
-	private boolean closed = false;
-	private E previous = null;
+	private static enum State {
+		//
+		WAITING, READY, FETCHED, CLOSED,
+		//
+		;
+	}
+
+	private State state = State.WAITING;
+
 	private E current = null;
 
 	private void assertOpen() {
-		if (this.closed) { throw new IllegalStateException("This iterator has already been closed"); }
+		if (this.state != State.CLOSED) { throw new NoSuchElementException("This iterator is already closed"); }
 	}
 
 	@Override
 	public final boolean hasNext() {
-		assertOpen();
-		boolean ret = (findNext() != null);
-		if (!ret) {
-			// If we've reached the last element, we close it
+		if (this.state == State.CLOSED) { return false; }
+		if (this.state == State.READY) { return true; }
+
+		// Either WAITING or FETCHED, we need to check to see if we have a next element
+		final boolean ret = checkNext();
+		if (ret) {
+			this.state = State.READY;
+		} else {
 			close();
 		}
 		return ret;
@@ -28,51 +39,67 @@ public abstract class CloseableIterator<E> implements Closeable, Iterator<E> {
 	@Override
 	public final E next() {
 		assertOpen();
-		E ret = findNext();
-		this.current = null;
-		this.previous = ret;
-		if (ret == null) { throw new NoSuchElementException(); }
-		return ret;
-	}
 
-	private final E findNext() {
-		assertOpen();
-		if (this.current == null) {
-			boolean ok = false;
-			try {
-				this.current = seek();
-				ok = true;
-			} catch (Throwable t) {
-				throw new RuntimeException("Failed to find the next element", t);
-			} finally {
-				if (!ok) {
-					close();
-				}
-			}
+		if (!hasNext()) {
+			close();
 		}
-		return this.current;
+		try {
+			this.current = getNext();
+			this.state = State.FETCHED;
+			return this.current;
+		} catch (Exception e) {
+			close();
+			throw new RuntimeException("Failed to fetch the next item in the iterator, closed automatically", e);
+		}
 	}
 
-	protected abstract E seek() throws Throwable;
+	/**
+	 * <p>
+	 * Test to see whether more elements are available. This is analogous to the traditional
+	 * {@link Iterator#hasNext()} method.
+	 * </p>
+	 *
+	 * @return {@code true} if there's another element to iterate over, {@code false} otherwise.
+	 */
+	protected abstract boolean checkNext();
+
+	/**
+	 * <p>
+	 * Retrieve the next element in the collection, or raise a {@link NoSuchElementException} if no
+	 * element exists. This method is analogous to the traditional {@link Iterator#next()} method.
+	 * </p>
+	 *
+	 * @return the next element in the iteration
+	 * @throws NoSuchElementException
+	 *             if no further elements exist
+	 * @throws Exception
+	 *             any exception raised by the actual seek process. This will cause the iterator to
+	 *             be closed
+	 */
+	protected abstract E getNext() throws Exception;
 
 	@Override
 	public final void remove() {
 		assertOpen();
-		if (this.previous == null) { throw new IllegalStateException("No element to remove"); }
-		remove(this.previous);
+		if (this.state != State.FETCHED) { throw new IllegalStateException("No element to remove"); }
+		try {
+			remove(this.current);
+		} finally {
+			this.state = State.WAITING;
+		}
 	}
 
-	protected void remove(E next) {
+	protected void remove(E current) {
 		throw new UnsupportedOperationException("Remove not supported for this iterator");
 	}
 
 	@Override
 	public final void close() {
-		if (this.closed) { return; }
+		if (this.state == State.CLOSED) { return; }
 		try {
 			doClose();
 		} finally {
-			this.closed = true;
+			this.state = State.CLOSED;
 		}
 	}
 
