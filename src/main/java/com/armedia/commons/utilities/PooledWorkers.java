7ec1e895460ad2f2807ca785e97bc7d6e4b16624
Diego Rivera <diego.rivera@armedia.com>
2017-12-15 17:47:01 -0600

    Modified the PooledWorkers to support a slightly cleaner synchronization interface wherein locks are used instead of the synchronized keyword. Some enhancements to facilitate usage.

--- src/main/java/com/armedia/commons/utilities/PooledWorkers.java
+++ src/main/java/com/armedia/commons/utilities/PooledWorkers.java
@@ -12,6 +12,9 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,6 +26,7 @@ public abstract class PooledWorkers<S, Q> {
 	private final List<Future<?>> futures;
 	private final AtomicInteger activeCounter;
 
+	private final ReadWriteLock lock = new ReentrantReadWriteLock();
 	private Q exitValue = null;
 	private int threadCount = 0;
 	private ThreadPoolExecutor executor = null;
@@ -40,6 +44,10 @@ public abstract class PooledWorkers<S, Q> {
 		private Worker(boolean blocking, Q exitValue) {
 			this.exitValue = exitValue;
 			this.blocking = blocking;
+		}
+
+		private boolean isExitValue(Object o) {
+			return ((this.exitValue != null) && (o == this.exitValue));
 		}
 
 		@Override
@@ -78,7 +86,7 @@ public abstract class PooledWorkers<S, Q> {
 					// looks the same out of some unfortunate coincidence. By checking
 					// instances, we ensure that we will not exit the loop prematurely
 					// due to a value collision.
-					if (next == this.exitValue) {
+					if (isExitValue(next)) {
 						// Work complete
 						this.log.debug("Exiting the work polling loop");
 						return;
@@ -118,124 +126,192 @@ public abstract class PooledWorkers<S, Q> {
 	protected abstract void cleanup(S state);
 
 	public final void addWorkItem(Q item) throws InterruptedException {
-		this.workQueue.put(item);
+		final Lock l = this.lock.readLock();
+		l.lock();
+		try {
+			this.workQueue.put(item);
+		} finally {
+			l.unlock();
+		}
+	}
+
+	public final boolean addWorkItemNonblock(Q item) {
+		final Lock l = this.lock.readLock();
+		l.lock();
+		try {
+			return this.workQueue.offer(item);
+		} finally {
+			l.unlock();
+		}
 	}
 
 	public final synchronized List<Q> clearWorkItems() {
-		List<Q> ret = new ArrayList<Q>();
-		this.workQueue.drainTo(ret);
-		return ret;
-	}
-
-	public final synchronized boolean start(int threadCount, Q exitValue, boolean blocking) {
-		if (this.executor != null) { return false; }
-		if (blocking
-			&& (exitValue == null)) { throw new IllegalArgumentException("Blocking mode requires an exit value"); }
-		this.threadCount = threadCount;
-		this.exitValue = exitValue;
-		this.activeCounter.set(0);
-		this.futures.clear();
-		Worker worker = new Worker(blocking, exitValue);
-		this.executor = new ThreadPoolExecutor(threadCount, threadCount, 30, TimeUnit.SECONDS,
-			new LinkedBlockingQueue<Runnable>());
-		for (int i = 0; i < this.threadCount; i++) {
-			this.futures.add(this.executor.submit(worker));
-		}
-		this.executor.shutdown();
-		return true;
-	}
-
-	private final synchronized void waitCleanly() {
-		this.log.debug("Signaling work completion for the workers");
-		boolean waitCleanly = true;
-		if (this.exitValue != null) {
-			for (int i = 0; i < this.threadCount; i++) {
-				try {
-					this.workQueue.put(this.exitValue);
-				} catch (InterruptedException e) {
-					waitCleanly = false;
-					// Here we have a problem: we're timing out while adding the exit
-					// values...
-					this.log.warn("Interrupted while attempting to request executor thread termination", e);
-					Thread.currentThread().interrupt();
-					this.executor.shutdownNow();
-					break;
-				}
-			}
-		}
-
+		final Lock l = this.lock.readLock();
+		l.lock();
 		try {
-			// We're done, we must wait until all workers are waiting
-			if (waitCleanly) {
-				this.log.debug(String.format("Waiting for %d workers to finish processing", this.threadCount));
-				for (Future<?> future : this.futures) {
+			List<Q> ret = new ArrayList<Q>();
+			this.workQueue.drainTo(ret);
+			return ret;
+		} finally {
+			l.unlock();
+		}
+	}
+
+	public final int getQueueSize() {
+		final Lock l = this.lock.readLock();
+		l.lock();
+		try {
+			return this.workQueue.size();
+		} finally {
+			l.unlock();
+		}
+	}
+
+	public final int getQueueCapacity() {
+		final Lock l = this.lock.readLock();
+		l.lock();
+		try {
+			return this.workQueue.remainingCapacity();
+		} finally {
+			l.unlock();
+		}
+	}
+
+	public final boolean start(int threadCount, Q exitValue) {
+		return start(threadCount, exitValue, true);
+	}
+
+	public final boolean start(int threadCount) {
+		return start(threadCount, null, false);
+	}
+
+	public final boolean start(int threadCount, Q exitValue, boolean blocking) {
+		final Lock l = this.lock.writeLock();
+		l.lock();
+		try {
+			if (this.executor != null) { return false; }
+			if (blocking
+				&& (exitValue == null)) { throw new IllegalArgumentException("Blocking mode requires an exit value"); }
+			this.threadCount = threadCount;
+			this.exitValue = exitValue;
+			this.activeCounter.set(0);
+			this.futures.clear();
+			Worker worker = new Worker(exitValue);
+			this.executor = new ThreadPoolExecutor(threadCount, threadCount, 30, TimeUnit.SECONDS,
+				new LinkedBlockingQueue<Runnable>());
+			for (int i = 0; i < this.threadCount; i++) {
+				this.futures.add(this.executor.submit(worker));
+			}
+			this.executor.shutdown();
+			return true;
+		} finally {
+			l.unlock();
+		}
+	}
+
+	private final List<Q> waitCleanly() {
+		final Lock l = this.lock.writeLock();
+		l.lock();
+		try {
+			this.log.debug("Signaling work completion for the workers");
+			boolean waitCleanly = true;
+			if (this.exitValue != null) {
+				for (int i = 0; i < this.threadCount; i++) {
 					try {
-						future.get();
+						this.workQueue.put(this.exitValue);
 					} catch (InterruptedException e) {
-						this.log.warn("Interrupted while waiting for an executor thread to exit, forcing the shutdown",
-							e);
+						waitCleanly = false;
+						// Here we have a problem: we're timing out while adding the exit
+						// values...
+						this.log.warn("Interrupted while attempting to request executor thread termination", e);
 						Thread.currentThread().interrupt();
 						this.executor.shutdownNow();
 						break;
-					} catch (ExecutionException e) {
-						this.log.warn("An executor thread raised an exception", e);
-					} catch (CancellationException e) {
-						this.log.warn("An executor thread was canceled!", e);
 					}
 				}
-				this.log.debug("All the workers are done.");
 			}
-		} finally {
-			List<Q> remaining = new ArrayList<Q>();
-			this.workQueue.drainTo(remaining);
-			for (Q v : remaining) {
-				if ((this.exitValue != null) && (v == this.exitValue)) {
-					continue;
-				}
-				this.log.error(String.format("WORK LEFT PENDING IN THE QUEUE: %s", v));
-			}
-			remaining.clear();
-		}
 
-		// If there are still pending workers, then wait for them to finish for up to 5
-		// minutes
-		int pending = this.activeCounter.get();
-		if (pending > 0) {
-			this.log.debug(String
-				.format("Waiting for pending workers to terminate (maximum 5 minutes, %d pending workers)", pending));
+			List<Q> remaining = new ArrayList<Q>();
 			try {
-				this.executor.awaitTermination(5, TimeUnit.MINUTES);
-			} catch (InterruptedException e) {
-				this.log.warn("Interrupted while waiting for normal executor termination", e);
-				Thread.currentThread().interrupt();
+				// We're done, we must wait until all workers are waiting
+				if (waitCleanly) {
+					this.log.debug(String.format("Waiting for %d workers to finish processing", this.threadCount));
+					for (Future<?> future : this.futures) {
+						try {
+							future.get();
+						} catch (InterruptedException e) {
+							this.log.warn(
+								"Interrupted while waiting for an executor thread to exit, forcing the shutdown", e);
+							Thread.currentThread().interrupt();
+							this.executor.shutdownNow();
+							break;
+						} catch (ExecutionException e) {
+							this.log.warn("An executor thread raised an exception", e);
+						} catch (CancellationException e) {
+							this.log.warn("An executor thread was canceled!", e);
+						}
+					}
+					this.log.debug("All the workers are done.");
+				}
+			} finally {
+				this.workQueue.drainTo(remaining);
+				for (Q v : remaining) {
+					if ((this.exitValue != null) && (v == this.exitValue)) {
+						continue;
+					}
+					this.log.error(String.format("WORK LEFT PENDING IN THE QUEUE: %s", v));
+				}
 			}
+
+			// If there are still pending workers, then wait for them to finish for up to 5
+			// minutes
+			int pending = this.activeCounter.get();
+			if (pending > 0) {
+				this.log.debug(String.format(
+					"Waiting for pending workers to terminate (maximum 5 minutes, %d pending workers)", pending));
+				try {
+					this.executor.awaitTermination(5, TimeUnit.MINUTES);
+				} catch (InterruptedException e) {
+					this.log.warn("Interrupted while waiting for normal executor termination", e);
+					Thread.currentThread().interrupt();
+				}
+			}
+			return remaining;
+		} finally {
+			l.unlock();
 		}
 	}
 
-	public final synchronized void waitForCompletion() {
-		if (this.executor == null) { return; }
+	public final void waitForCompletion() {
+		final Lock l = this.lock.writeLock();
+		l.lock();
 		try {
-			waitCleanly();
-		} finally {
+			if (this.executor == null) { return; }
 			try {
-				this.executor.shutdownNow();
-				int pending = this.activeCounter.get();
-				if (pending > 0) {
-					try {
-						this.log.debug(String.format(
-							"Waiting an additional 60 seconds for worker termination as a contingency (%d pending workers)",
-							pending));
-						this.executor.awaitTermination(1, TimeUnit.MINUTES);
-					} catch (InterruptedException e) {
-						this.log.warn("Interrupted while waiting for immediate executor termination", e);
-						Thread.currentThread().interrupt();
-					}
-				}
+				waitCleanly();
 			} finally {
-				this.executor = null;
-				this.threadCount = 0;
-				this.exitValue = null;
+				try {
+					this.executor.shutdownNow();
+					int pending = this.activeCounter.get();
+					if (pending > 0) {
+						try {
+							this.log.debug(String.format(
+								"Waiting an additional 60 seconds for worker termination as a contingency (%d pending workers)",
+								pending));
+							this.executor.awaitTermination(1, TimeUnit.MINUTES);
+						} catch (InterruptedException e) {
+							this.log.warn("Interrupted while waiting for immediate executor termination", e);
+							Thread.currentThread().interrupt();
+						}
+					}
+				} finally {
+					this.executor = null;
+					this.threadCount = 0;
+					this.exitValue = null;
+				}
 			}
+		} finally {
+			l.unlock();
 		}
 	}
 }
