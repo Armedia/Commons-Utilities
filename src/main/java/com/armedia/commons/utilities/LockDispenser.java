ac25c036d8a8d91a8069fb853bf51b4643b4be41
Diego Rivera <diego.rivera@armedia.com>
2019-01-21 09:05:00 -0600

    Removed some unnecessary functional interfaces, and some cleanup

--- src/main/java/com/armedia/commons/utilities/LockDispenser.java
+++ src/main/java/com/armedia/commons/utilities/LockDispenser.java
@@ -19,6 +19,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.function.Function;
 
 import org.apache.commons.lang3.concurrent.ConcurrentException;
 import org.apache.commons.lang3.concurrent.ConcurrentInitializer;
@@ -30,56 +31,40 @@ import org.apache.commons.lang3.concurrent.ConcurrentUtils;
  */
 public final class LockDispenser<K, C> {
 
-	@FunctionalInterface
-	public static interface LockBuilder<K, C> {
-		public C newLock(K key);
+	public static <C> Function<C, Reference<C>> weakReferenceBuilder() {
+		return (v) -> new WeakReference<>(v);
 	}
 
-	@FunctionalInterface
-	public static interface ReferenceBuilder<C> {
-		public Reference<C> newReference(C key);
-	}
-
-	public static <C> ReferenceBuilder<C> getWeakReferenceBuilder() {
-		return (v) -> {
-			return new WeakReference<>(v);
-		};
-	}
-
-	public static <C> ReferenceBuilder<C> getSoftReferenceBuilder() {
-		return (v) -> {
-			return new SoftReference<>(v);
-		};
+	public static <C> Function<C, Reference<C>> softReferenceBuilder() {
+		return (v) -> new SoftReference<>(v);
 	}
 
 	/**
 	 * <p>
-	 * Deprecated in favor of {@link #getSynchronized()}
+	 * Deprecated in favor of {@link #synchronizableObject()}
 	 * </p>
 	 *
-	 * @return the same as {@link #getSynchronized()}
+	 * @return the same as {@link #synchronizableObject()}
 	 */
 	@Deprecated
 	public static <K> LockDispenser<K, Object> getBasic() {
-		return LockDispenser.getSynchronized();
+		return LockDispenser.basic();
 	}
 
-	public static <K> LockDispenser<K, Object> getSynchronized() {
-		return new LockDispenser<>((k) -> {
-			return new Object();
-		});
+	public static <K> LockDispenser<K, Object> basic() {
+		return LockDispenser.synchronizableObject();
 	}
 
-	public static <K> LockDispenser<K, ReentrantLock> getLock() {
-		return new LockDispenser<>((k) -> {
-			return new ReentrantLock();
-		});
+	public static <K> LockDispenser<K, Object> synchronizableObject() {
+		return new LockDispenser<>((k) -> new Object());
 	}
 
-	public static <K> LockDispenser<K, ReentrantReadWriteLock> getReadWriteLock() {
-		return new LockDispenser<>((k) -> {
-			return new ReentrantReadWriteLock();
-		});
+	public static <K> LockDispenser<K, ReentrantLock> reentrantLock() {
+		return new LockDispenser<>((k) -> new ReentrantLock());
+	}
+
+	public static <K> LockDispenser<K, ReentrantReadWriteLock> reentrantReadWriteLock() {
+		return new LockDispenser<>((k) -> new ReentrantReadWriteLock());
 	}
 
 	private class LockBox {
@@ -93,26 +78,29 @@ public final class LockDispenser<K, C> {
 
 		private synchronized C get() {
 			if ((this.lock == null) || (this.lock.get() == null)) {
-				C lock = LockDispenser.this.lockBuilder.newLock(this.key);
-				if (lock == null) { throw new RuntimeException(
-					"The LockBuilder must always return a non-null lock object instance"); }
-				this.lock = LockDispenser.this.referenceBuilder.newReference(lock);
-				if (this.lock == null) { throw new RuntimeException(
-					"The ReferenceBuilder must always return a non-null Reference<> instance"); }
+				C lock = LockDispenser.this.lockBuilder.apply(this.key);
+				if (lock == null) {
+					throw new RuntimeException("The LockBuilder must always return a non-null lock object instance");
+				}
+				this.lock = LockDispenser.this.referenceBuilder.apply(lock);
+				if (this.lock == null) {
+					throw new RuntimeException(
+						"The ReferenceBuilder must always return a non-null Reference<> instance");
+				}
 			}
 			return this.lock.get();
 		}
 	}
 
-	private final LockBuilder<K, C> lockBuilder;
-	private final ReferenceBuilder<C> referenceBuilder;
+	private final Function<K, C> lockBuilder;
+	private final Function<C, Reference<C>> referenceBuilder;
 	private final ConcurrentMap<K, LockBox> locks = new ConcurrentHashMap<>();
 
-	public LockDispenser(LockBuilder<K, C> lockBuilder) {
-		this(lockBuilder, LockDispenser.getWeakReferenceBuilder());
+	public LockDispenser(Function<K, C> lockBuilder) {
+		this(lockBuilder, LockDispenser.weakReferenceBuilder());
 	}
 
-	public LockDispenser(LockBuilder<K, C> lockBuilder, ReferenceBuilder<C> referenceBuilder) {
+	public LockDispenser(Function<K, C> lockBuilder, Function<C, Reference<C>> referenceBuilder) {
 		this.lockBuilder = Objects.requireNonNull(lockBuilder, "Must provide a non-null LockBuilder instance");
 		this.referenceBuilder = Objects.requireNonNull(referenceBuilder,
 			"Must provide a non-null ReferenceBuilder instance");
