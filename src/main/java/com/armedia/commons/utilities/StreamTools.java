01cd04ad71e00fb8a7f88114df6de81f14b61bf3
Diego Rivera <diego.rivera@armedia.com>
2019-01-24 07:52:02 -0600

    Somemore refactoring for easier naming

--- src/main/java/com/armedia/commons/utilities/StreamTools.java
+++ src/main/java/com/armedia/commons/utilities/StreamTools.java
@@ -11,19 +11,19 @@ public class StreamTools {
 	public static final int DEFAULT_CHARACTERISTICS = Spliterator.DISTINCT | Spliterator.IMMUTABLE | Spliterator.ORDERED;
 	public static final boolean DEFAULT_PARALLEL = false;
 
-	public static <T> Stream<T> streamFromIterator(Iterator<T> it) {
-		return StreamTools.streamFromIterator(it, StreamTools.DEFAULT_CHARACTERISTICS, StreamTools.DEFAULT_PARALLEL);
+	public static <T> Stream<T> fromIterator(Iterator<T> it) {
+		return StreamTools.fromIterator(it, StreamTools.DEFAULT_CHARACTERISTICS, StreamTools.DEFAULT_PARALLEL);
 	}
 
-	public static <T> Stream<T> streamFromIterator(Iterator<T> it, int characteristics) {
-		return StreamTools.streamFromIterator(it, characteristics, StreamTools.DEFAULT_PARALLEL);
+	public static <T> Stream<T> fromIterator(Iterator<T> it, int characteristics) {
+		return StreamTools.fromIterator(it, characteristics, StreamTools.DEFAULT_PARALLEL);
 	}
 
-	public static <T> Stream<T> streamFromIterator(Iterator<T> it, boolean parallel) {
-		return StreamTools.streamFromIterator(it, StreamTools.DEFAULT_CHARACTERISTICS, parallel);
+	public static <T> Stream<T> fromIterator(Iterator<T> it, boolean parallel) {
+		return StreamTools.fromIterator(it, StreamTools.DEFAULT_CHARACTERISTICS, parallel);
 	}
 
-	public static <T> Stream<T> streamFromIterator(Iterator<T> it, int characteristics, boolean parallel) {
+	public static <T> Stream<T> fromIterator(Iterator<T> it, int characteristics, boolean parallel) {
 		if (it == null) { return Stream.empty(); }
 		return StreamSupport.stream(Spliterators.spliteratorUnknownSize(it, characteristics), parallel);
 	}
