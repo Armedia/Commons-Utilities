64b6b9571fe8aa3b0645f31057151182ac769c14
Diego Rivera <diego.rivera@armedia.com>
2019-01-31 15:44:08 -0600

    Added a class that will help with complex formatting in log messages while retaining the ability to not execute complex formats when not necessary

---
+++ src/main/java/com/armedia/commons/utilities/LazyFormatter.java
@@ -0,0 +1,40 @@
+package com.armedia.commons.utilities;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+public final class LazyFormatter {
+
+	public static final String NL = String.format("%n");
+	private static final Object[] NO_ARGS = {};
+
+	private final Supplier<String> supplier;
+	private volatile String value = null;
+
+	private LazyFormatter(Supplier<String> str) {
+		this.supplier = Objects.requireNonNull(str, "Must provide a non-null String Supplier instance");
+	}
+
+	public static LazyFormatter lazyFormat(String format, Object... args) {
+		Objects.requireNonNull(format, "Must provide a non-null formatter string");
+		return LazyFormatter.lazyFormat(() -> String.format(format, Tools.coalesce(args, LazyFormatter.NO_ARGS)));
+	}
+
+	public static LazyFormatter lazyFormat(Supplier<String> str) {
+		return new LazyFormatter(str);
+	}
+
+	@Override
+	public String toString() {
+		String localValue = this.value;
+		if (localValue == null) {
+			synchronized (this) {
+				localValue = this.value;
+				if (localValue == null) {
+					this.value = localValue = this.supplier.get();
+				}
+			}
+		}
+		return localValue;
+	}
+}
