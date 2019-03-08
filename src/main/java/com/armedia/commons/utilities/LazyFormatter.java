068a699ab938e38499f61d10459b15c362e7f42d
Diego Rivera <diego.rivera@armedia.com>
2019-03-08 13:14:10 -0600

    Minor optimizations

--- src/main/java/com/armedia/commons/utilities/LazyFormatter.java
+++ src/main/java/com/armedia/commons/utilities/LazyFormatter.java
@@ -3,9 +3,7 @@ package com.armedia.commons.utilities;
 import java.util.Objects;
 import java.util.function.Supplier;
 
-public final class LazyFormatter {
-
-	private static final Object[] NO_ARGS = {};
+public final class LazyFormatter implements Supplier<String> {
 
 	private final Supplier<String> supplier;
 	private volatile String value = null;
@@ -14,13 +12,24 @@ public final class LazyFormatter {
 		this.supplier = Objects.requireNonNull(str, "Must provide a non-null String Supplier instance");
 	}
 
+	private LazyFormatter(String str) {
+		this.value = Objects.requireNonNull(str, "Must provide a non-null String instance");
+		this.supplier = null;
+	}
+
 	public static LazyFormatter lazyFormat(String format, Object... args) {
 		Objects.requireNonNull(format, "Must provide a non-null formatter string");
-		return LazyFormatter.lazyFormat(() -> String.format(format, Tools.coalesce(args, LazyFormatter.NO_ARGS)));
+		if ((args == null) || (args.length == 0)) { return LazyFormatter.lazyFormat(format); }
+		return LazyFormatter.lazyFormat(() -> String.format(format, args));
 	}
 
 	public static LazyFormatter lazyFormat(Supplier<String> str) {
 		return new LazyFormatter(str);
+	}
+
+	@Override
+	public String get() {
+		return toString();
 	}
 
 	@Override
