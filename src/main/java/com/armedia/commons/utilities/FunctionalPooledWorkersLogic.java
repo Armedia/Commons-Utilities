d28219f6aab5aeffba29db6cf901d87be908fac6
Diego Rivera <diego.rivera@armedia.com>
2019-03-24 10:40:00 -0600

    Updated to support both checked and unchecked functional objects

--- src/main/java/com/armedia/commons/utilities/FunctionalPooledWorkersLogic.java
+++ src/main/java/com/armedia/commons/utilities/FunctionalPooledWorkersLogic.java
@@ -5,6 +5,9 @@ import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
+import com.armedia.commons.utilities.function.CheckedBiConsumer;
+import com.armedia.commons.utilities.function.CheckedSupplier;
+import com.armedia.commons.utilities.function.CheckedTools;
 import com.armedia.commons.utilities.function.TriConsumer;
 
 /**
@@ -21,8 +24,8 @@ import com.armedia.commons.utilities.function.TriConsumer;
 public final class FunctionalPooledWorkersLogic<STATE, ITEM, EX extends Throwable>
 	implements PooledWorkersLogic<STATE, ITEM, EX> {
 
-	private final Supplier<STATE> initializer;
-	private final BiConsumer<STATE, ITEM> processor;
+	private final CheckedSupplier<STATE, EX> initializer;
+	private final CheckedBiConsumer<STATE, ITEM, EX> processor;
 	private final TriConsumer<STATE, ITEM, EX> failureHandler;
 	private final Consumer<STATE> cleanup;
 
@@ -60,13 +63,52 @@ public final class FunctionalPooledWorkersLogic<STATE, ITEM, EX extends Throwabl
 
 	public FunctionalPooledWorkersLogic(Supplier<STATE> initializer, BiConsumer<STATE, ITEM> processor,
 		TriConsumer<STATE, ITEM, EX> failureHandler, Consumer<STATE> cleanup) {
+		this(CheckedTools.check(initializer), CheckedTools.check(processor), failureHandler, cleanup);
+	}
+
+	public FunctionalPooledWorkersLogic(CheckedBiConsumer<STATE, ITEM, EX> processor) {
+		this(null, processor, null, null);
+	}
+
+	public FunctionalPooledWorkersLogic(CheckedSupplier<STATE, EX> initializer,
+		CheckedBiConsumer<STATE, ITEM, EX> processor) {
+		this(initializer, processor, null, null);
+	}
+
+	public FunctionalPooledWorkersLogic(CheckedBiConsumer<STATE, ITEM, EX> processor, Consumer<STATE> cleanup) {
+		this(null, processor, null, cleanup);
+	}
+
+	public FunctionalPooledWorkersLogic(CheckedSupplier<STATE, EX> initializer,
+		CheckedBiConsumer<STATE, ITEM, EX> processor, Consumer<STATE> cleanup) {
+		this(initializer, processor, null, cleanup);
+	}
+
+	public FunctionalPooledWorkersLogic(CheckedBiConsumer<STATE, ITEM, EX> processor,
+		TriConsumer<STATE, ITEM, EX> failureHandler) {
+		this(null, processor, failureHandler, null);
+	}
+
+	public FunctionalPooledWorkersLogic(CheckedSupplier<STATE, EX> initializer,
+		CheckedBiConsumer<STATE, ITEM, EX> processor, TriConsumer<STATE, ITEM, EX> failureHandler) {
+		this(initializer, processor, failureHandler, null);
+	}
+
+	public FunctionalPooledWorkersLogic(CheckedBiConsumer<STATE, ITEM, EX> processor,
+		TriConsumer<STATE, ITEM, EX> failureHandler, Consumer<STATE> cleanup) {
+		this(null, processor, failureHandler, cleanup);
+	}
+
+	public FunctionalPooledWorkersLogic(CheckedSupplier<STATE, EX> initializer,
+		CheckedBiConsumer<STATE, ITEM, EX> processor, TriConsumer<STATE, ITEM, EX> failureHandler,
+		Consumer<STATE> cleanup) {
 		this.processor = Objects.requireNonNull(processor, "Must provide a non-null BiConsumer to process the items");
 		this.initializer = Tools.coalesce(initializer, this::nullInitialize);
 		this.failureHandler = Tools.coalesce(failureHandler, this::nullHandleFailure);
 		this.cleanup = Tools.coalesce(cleanup, this::nullCleanup);
 	}
 
-	private STATE nullInitialize() {
+	private STATE nullInitialize() throws EX {
 		// Do nothing
 		return null;
 	}
