170fb3cbacc261677ccc0c17859ce8b63a595043
Diego Rivera <diego.rivera@armedia.com>
2019-05-10 15:12:25 -0600

    Made classes abstract, as they should be

--- src/main/java/com/armedia/commons/utilities/xml/EnumToStringAnyElementMapAdapter.java
+++ src/main/java/com/armedia/commons/utilities/xml/EnumToStringAnyElementMapAdapter.java
@@ -1,6 +1,6 @@
 package com.armedia.commons.utilities.xml;
 
-public class EnumToStringAnyElementMapAdapter<E extends Enum<E>> extends AnyElementMapAdapter<E, String> {
+public abstract class EnumToStringAnyElementMapAdapter<E extends Enum<E>> extends AnyElementMapAdapter<E, String> {
 	public EnumToStringAnyElementMapAdapter(AnyElementCodec<E> enumCodec) {
 		super(enumCodec, AnyElementCodec.STRING);
 	}
--- src/main/java/com/armedia/commons/utilities/xml/StringToEnumAnyElementMapAdapter.java
+++ src/main/java/com/armedia/commons/utilities/xml/StringToEnumAnyElementMapAdapter.java
@@ -1,6 +1,6 @@
 package com.armedia.commons.utilities.xml;
 
-public class StringToEnumAnyElementMapAdapter<E extends Enum<E>> extends AnyElementMapAdapter<String, E> {
+public abstract class StringToEnumAnyElementMapAdapter<E extends Enum<E>> extends AnyElementMapAdapter<String, E> {
 	public StringToEnumAnyElementMapAdapter(AnyElementCodec<E> enumCodec) {
 		super(AnyElementCodec.STRING, enumCodec);
 	}
