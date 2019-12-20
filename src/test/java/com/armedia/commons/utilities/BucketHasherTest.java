3eed02a9c1a72be83a94650b300f04686d7d4723
Diego Rivera <diego.rivera@armedia.com>
2019-12-20 11:54:53 -0600

    More testing for the hash function to match other implementations

--- src/test/java/com/armedia/commons/utilities/BucketHasherTest.java
+++ src/test/java/com/armedia/commons/utilities/BucketHasherTest.java
@@ -4,7 +4,10 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
+import java.util.Collection;
+import java.util.LinkedList;
 
+import org.apache.commons.lang3.tuple.Triple;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -43,6 +46,42 @@ public class BucketHasherTest {
 		Assertions.assertEquals(0L, BucketHasher.hash(buf, 0, 0));
 		Assertions.assertEquals(0L, BucketHasher.hash(in, 0));
 		Assertions.assertEquals(0L, BucketHasher.hash(in, 0, 0));
+
+		Assertions.assertThrows(IllegalArgumentException.class, () -> BucketHasher.hash(1, Long.MIN_VALUE));
+		Assertions.assertThrows(IllegalArgumentException.class, () -> BucketHasher.hash(1, -1L));
+		Assertions.assertThrows(IllegalArgumentException.class, () -> BucketHasher.hash(1, 4294967296L));
+		Assertions.assertThrows(IllegalArgumentException.class, () -> BucketHasher.hash(1, Long.MAX_VALUE));
+
+		Assertions.assertThrows(IllegalArgumentException.class,
+			() -> BucketHasher.hash(1, BucketHasher.DEF_BUCKET, Long.MIN_VALUE));
+		Assertions.assertThrows(IllegalArgumentException.class,
+			() -> BucketHasher.hash(1, BucketHasher.DEF_BUCKET, -1L));
+		Assertions.assertThrows(IllegalArgumentException.class,
+			() -> BucketHasher.hash(1, BucketHasher.DEF_BUCKET, 4294967296L));
+		Assertions.assertThrows(IllegalArgumentException.class,
+			() -> BucketHasher.hash(1, BucketHasher.DEF_BUCKET, Long.MAX_VALUE));
+		Assertions.assertEquals(3509391659L, BucketHasher.hash(1, BucketHasher.DEF_BUCKET, BucketHasher.DEF_SEED));
+		Assertions.assertEquals(2441322222L, BucketHasher.hash(1, BucketHasher.DEF_BUCKET, 10L));
+		Assertions.assertEquals(3501171530L, BucketHasher.hash(1, BucketHasher.DEF_BUCKET, BucketHasher.MAX_SEED));
+
+		Collection<Triple<String, Long, Class<Throwable>>> c = new LinkedList<>();
+		c.add(Triple.of("010203", 2665938722L, null));
+		c.add(Triple.of("abc", 1191608682L, null));
+		for (Triple<String, Long, Class<Throwable>> r : c) {
+			Class<Throwable> t = r.getRight();
+			if (t != null) {
+				Assertions.assertThrows(t, () -> BucketHasher.hash(r.getLeft()));
+				continue;
+			} else {
+				Assertions.assertEquals(r.getMiddle(), BucketHasher.hash(r.getLeft()), r.getLeft());
+			}
+		}
+		b = new byte[3];
+		b[0] = 1;
+		b[1] = 2;
+		b[2] = 3;
+		Assertions.assertEquals(2562861693L, BucketHasher.hash(b));
+
 	}
 
 }
