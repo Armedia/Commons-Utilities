a0409258c41e00f12bf55b50de4d6ead301ffd2b
Diego Rivera <diego.rivera@armedia.com>
2020-03-16 10:20:43 -0600

    Minor fix to reduce unnecessary processing

--- src/main/java/com/armedia/commons/utilities/Tools.java
+++ src/main/java/com/armedia/commons/utilities/Tools.java
@@ -2207,14 +2207,19 @@ public class Tools {
 
 	public static Path toRealPath(Path p, boolean followLinks) {
 		if (p == null) { return null; }
+		boolean normalize = false;
 		try {
 			p = p.toRealPath((followLinks ? Tools.FOLLOW_LINKS : Tools.NOFOLLOW_LINKS));
 		} catch (IOException e) {
 			// Ignore...
+			normalize = true;
 		} finally {
+			if (normalize) {
+				p = p.normalize();
+			}
 			p = p.toAbsolutePath();
 		}
-		return p.normalize();
+		return p;
 	}
 
 	public static Path canonicalize(Path p) {
