b722e1480c046b62b1320093f5682f4f37c9373d
Diego Rivera <diego.rivera@armedia.com>
2020-05-23 09:31:27 -0600

    Some optimization and enhancement for collections compatibility

--- src/main/java/com/armedia/commons/utilities/CloseUtils.java
+++ src/main/java/com/armedia/commons/utilities/CloseUtils.java
@@ -1,36 +1,51 @@
 package com.armedia.commons.utilities;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Objects;
 import java.util.function.BiConsumer;
 
 import org.slf4j.Logger;
 
 public class CloseUtils {
-	private static final BiConsumer<String, Object[]> NULL = null;
+
+	private static BiConsumer<String, Object[]> getConsumer(Logger log) {
+		return (log != null ? log::error : null);
+	}
 
 	public static void closeQuietly(AutoCloseable... closeables) {
-		CloseUtils.closeQuietly(CloseUtils.NULL, closeables);
+		CloseUtils.closeQuietly(CloseUtils.getConsumer(null), closeables);
+	}
+
+	public static void closeQuietly(Collection<AutoCloseable> closeables) {
+		CloseUtils.closeQuietly(CloseUtils.getConsumer(null), closeables);
 	}
 
 	public static void closeQuietly(Logger log, AutoCloseable... closeables) {
-		BiConsumer<String, Object[]> l = (log != null ? log::error : null);
-		CloseUtils.closeQuietly(l, closeables);
+		CloseUtils.closeQuietly(CloseUtils.getConsumer(log), closeables);
+	}
+
+	public static void closeQuietly(Logger log, Collection<AutoCloseable> closeables) {
+		CloseUtils.closeQuietly(CloseUtils.getConsumer(log), closeables);
 	}
 
 	public static void closeQuietly(BiConsumer<String, Object[]> log, AutoCloseable... closeables) {
-		if ((closeables == null) || (closeables.length == 0)) { return; }
+		CloseUtils.closeQuietly(log, (closeables != null ? Arrays.asList(closeables) : null));
+	}
+
+	public static void closeQuietly(BiConsumer<String, Object[]> log, Collection<AutoCloseable> closeables) {
+		if ((closeables == null) || (closeables.isEmpty())) { return; }
 		final Object[] args = (log != null ? new Object[2] : null);
-		for (AutoCloseable c : closeables) {
-			if (c != null) {
-				try {
-					c.close();
-				} catch (Exception e) {
-					if (log != null) {
-						args[0] = c.getClass();
-						args[1] = e;
-						log.accept("Exception caught while closing an AutoCloseable resource of {}", args);
-					}
+		closeables.stream().filter(Objects::nonNull).forEach((c) -> {
+			try {
+				c.close();
+			} catch (Exception e) {
+				if (log != null) {
+					args[0] = c.getClass();
+					args[1] = e;
+					log.accept("Exception caught while closing an AutoCloseable resource of {}", args);
 				}
 			}
-		}
+		});
 	}
 }
