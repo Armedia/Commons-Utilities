cde662bf3157095b2119c3040fb363bf9df1833a
Diego Rivera <diego.rivera@armedia.com>
2020-06-04 07:48:50 -0600

    Added the missing license

--- src/main/java/com/armedia/commons/utilities/io/NullWritableByteChannel.java
+++ src/main/java/com/armedia/commons/utilities/io/NullWritableByteChannel.java
@@ -1,27 +1,27 @@
 /*******************************************************************************
  * #%L
- * Armedia ArkCase
+ * Armedia Caliente
  * %%
- * Copyright (C) 2013 - 2019 Armedia, LLC
+ * Copyright (C) 2013 - 2020 Armedia, LLC
  * %%
- * This file is part of the ArkCase software.
+ * This file is part of the Caliente software.
  *
- * If the software was purchased under a paid ArkCase license, the terms of
+ * If the software was purchased under a paid Caliente license, the terms of
  * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
  *
- * ArkCase is free software: you can redistribute it and/or modify
+ * Caliente is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * ArkCase is distributed in the hope that it will be useful,
+ * Caliente is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
+ * along with Caliente. If not, see <http://www.gnu.org/licenses/>.
  * #L%
  *******************************************************************************/
 package com.armedia.commons.utilities.io;
