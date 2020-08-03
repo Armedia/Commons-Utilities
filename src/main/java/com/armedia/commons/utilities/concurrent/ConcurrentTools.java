5a340c02eceb66e2684b6ac4a954a5da833c622e
Diego Rivera <diego.rivera@armedia.com>
2020-08-03 10:08:43 -0600

    BUGFIX: Use applyChecked() to have the correct exception bubble upwards

--- src/main/java/com/armedia/commons/utilities/concurrent/ConcurrentTools.java
+++ src/main/java/com/armedia/commons/utilities/concurrent/ConcurrentTools.java
@@ -5,21 +5,21 @@
  * Copyright (C) 2013 - 2020 Armedia, LLC
  * %%
  * This file is part of the Caliente software.
- * 
+ *
  * If the software was purchased under a paid Caliente license, the terms of
  * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * Caliente is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Caliente is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Caliente. If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -48,7 +48,7 @@ public class ConcurrentTools {
 		if ((map == null) || (initializer == null)) { return null; }
 
 		final V value = map.get(key);
-		if (value == null) { return ConcurrentUtils.putIfAbsent(map, key, initializer.apply(key)); }
+		if (value == null) { return ConcurrentUtils.putIfAbsent(map, key, initializer.applyChecked(key)); }
 		return value;
 	}
 
