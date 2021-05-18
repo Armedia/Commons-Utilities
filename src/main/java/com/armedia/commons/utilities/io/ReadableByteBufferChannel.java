63bf84c010e239c2032cb9723c9b304a13f45b15
Diego Rivera <diego.rivera@armedia.com>
2021-05-18 13:46:30 -0600

    Added a utility class to facilitate wrapping byte buffers in channels

---
+++ src/main/java/com/armedia/commons/utilities/io/ReadableByteBufferChannel.java
@@ -0,0 +1,74 @@
+/*-
+ * #%L
+ * Armedia Commons Utilities
+ * %%
+ * Copyright (C) 2013 - 2021 Armedia, LLC
+ * %%
+ * This file is part of the Caliente software. 
+ * 
+ * If the software was purchased under a paid Caliente license, the terms of 
+ * the paid license agreement will prevail.  Otherwise, the software is 
+ * provided under the following open source license terms:
+ * 
+ * Caliente is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * Caliente is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Caliente. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+package com.armedia.commons.utilities.io;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.ClosedChannelException;
+import java.nio.channels.ReadableByteChannel;
+import java.util.Objects;
+
+import com.armedia.commons.utilities.concurrent.BaseShareableLockable;
+import com.armedia.commons.utilities.concurrent.MutexAutoLock;
+
+public class ReadableByteBufferChannel extends BaseShareableLockable implements ReadableByteChannel {
+
+	private final ByteBuffer data;
+	private volatile boolean open = true;
+
+	public ReadableByteBufferChannel(ByteBuffer data) {
+		this.data = Objects.requireNonNull(data, "Must provide a non-null ByteBuffer");
+	}
+
+	@Override
+	public boolean isOpen() {
+		return shareLocked(() -> this.open);
+	}
+
+	@Override
+	public void close() {
+		shareLockedUpgradable(() -> this.open, () -> {
+			this.open = false;
+		});
+	}
+
+	@Override
+	public int read(ByteBuffer dst) throws IOException {
+		try (MutexAutoLock mutex = autoMutexLock()) {
+			if (!this.open) { throw new ClosedChannelException(); }
+			if (!this.data.hasRemaining()) { return -1; }
+
+			int remaining = dst.remaining();
+			int written = 0;
+			while ((written < remaining) && this.data.hasRemaining()) {
+				dst.put(this.data.get());
+				written++;
+			}
+			return written;
+		}
+	}
+}
