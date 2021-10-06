e5d3db09e1b1cfab9f73bb8919b8a0c877617a0a
Diego Rivera <diego.rivera@armedia.com>
2021-10-06 13:00:12 -0600

    Some changes for Java 11 compatibility

--- src/main/java/com/armedia/commons/utilities/cli/launcher/log/Log4JConfigurator.java
+++ src/main/java/com/armedia/commons/utilities/cli/launcher/log/Log4JConfigurator.java
@@ -5,30 +5,31 @@
  * Copyright (C) 2013 - 2021 Armedia, LLC
  * %%
  * This file is part of the Caliente software.
- * 
+ *
  * If the software was purchased under a paid Caliente license, the terms of
  * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * Caliente is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Caliente is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Caliente. If not, see <http://www.gnu.org/licenses/>.
  * #L%
  *******************************************************************************/
 package com.armedia.commons.utilities.cli.launcher.log;
 
+import java.net.URISyntaxException;
 import java.net.URL;
 
-import org.apache.log4j.xml.DOMConfigurator;
+import org.apache.logging.log4j.core.config.Configurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,7 +42,12 @@ class Log4JConfigurator extends LogConfigurator {
 		if (config == null) {
 			throw new RuntimeException("Failed to configure the boot log - no Log4J boot configuration was found");
 		}
-		DOMConfigurator.configure(config);
+		try {
+			Configurator.reconfigure(config.toURI());
+		} catch (URISyntaxException e) {
+			throw new RuntimeException("Failed to configure the boot log - the configuration URL [" + config
+				+ "] could not be converted to a valid URI", e);
+		}
 		return LoggerFactory.getLogger(LogConfigurator.DEFAULT_LOG_NAME);
 	}
 
