8156a96d702395f9d4be4a693a0b7991d0dcc79f
Diego Rivera <diego.rivera@armedia.com>
2021-10-06 13:00:20 -0600

    Minor modification to remove a warning

--- src/main/java/com/armedia/commons/utilities/cli/launcher/log/BasicLogger.java
+++ src/main/java/com/armedia/commons/utilities/cli/launcher/log/BasicLogger.java
@@ -5,21 +5,21 @@
  * Copyright (C) 2013 - 2021 Armedia, LLC
  * %%
  * This file is part of the Caliente software.
- * 
+ *
  * If the software was purchased under a paid Caliente license, the terms of
  * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * Caliente is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Caliente is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Caliente. If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -40,7 +40,7 @@ import com.armedia.commons.utilities.Tools;
 public class BasicLogger extends MarkerIgnoringBase implements Logger {
 	private static final long serialVersionUID = 1L;
 
-	private static enum Level {
+	public static enum Level {
 		//
 		NONE, ERROR, WARN, INFO, DEBUG, TRACE,
 		//
