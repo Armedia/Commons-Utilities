6b46ed9fa565b6705283f66800152a15dc35d702
Diego Rivera <diego.rivera@armedia.com>
2021-10-18 14:39:24 -0600

    BUGFIX: NumericValueFilter was not properly checking logic

--- src/main/java/com/armedia/commons/utilities/cli/filter/NumericValueFilter.java
+++ src/main/java/com/armedia/commons/utilities/cli/filter/NumericValueFilter.java
@@ -5,21 +5,21 @@
  * Copyright (C) 2013 - 2021 Armedia, LLC
  * %%
  * This file is part of the Caliente software.
- * 
+ *
  * If the software was purchased under a paid Caliente license, the terms of
  * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * Caliente is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Caliente is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Caliente. If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -86,24 +86,24 @@ public abstract class NumericValueFilter<N extends Number> extends OptionValueFi
 
 	private final boolean isInRange(N n) {
 		// If no min, always be higher than the minimum
-		final int lo = (this.min != null ? compare(this.min, n) : 1);
+		final int lo = (this.min != null ? compare(this.min, n) : -1);
 		// If no max, always be lower than the maximum
 		final int hi = (this.max != null ? compare(n, this.max) : -1);
 
 		boolean ok = true;
 		if (ok && (this.min != null)) {
 			if (this.minInc) {
-				ok &= (lo >= 0);
+				ok &= (lo <= 0);
 			} else {
-				ok &= (lo > 0);
+				ok &= (lo < 0);
 			}
 		}
 
 		if (ok && (this.max != null)) {
 			if (this.maxInc) {
-				ok &= (0 >= hi);
+				ok &= (hi <= 0);
 			} else {
-				ok &= (0 > hi);
+				ok &= (hi < 0);
 			}
 		}
 
