6343f88c441677afe7053a68c7d51dd8244a57fd
Diego Rivera <diego.rivera@armedia.com>
2022-02-08 17:27:35 -0600

    Modified to facilitate callbacks to the PooledWorkers from the logic

--- src/main/java/com/armedia/commons/utilities/PooledWorkers.java
+++ src/main/java/com/armedia/commons/utilities/PooledWorkers.java
@@ -5,21 +5,21 @@
  * Copyright (C) 2013 - 2022 Armedia, LLC
  * %%
  * This file is part of the Caliente software.
- * 
+ *
  * If the software was purchased under a paid Caliente license, the terms of
  * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * Caliente is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Caliente is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Caliente. If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -63,8 +63,9 @@ import com.armedia.commons.utilities.concurrent.SharedAutoLock;
  * to be submitted and execute it as it arrives.
  *
  * @param <STATE>
- *            The state class that will be produced by {@link PooledWorkersLogic#initialize()} and
- *            consumed by {@link PooledWorkersLogic#process(Object, Object)} and
+ *            The state class that will be produced by
+ *            {@link PooledWorkersLogic#initialize(PooledWorkers)} and consumed by
+ *            {@link PooledWorkersLogic#process(Object, Object)} and
  *            {@link PooledWorkersLogic#cleanup(Object)}
  * @param <ITEM>
  *            The item class that will be processed by
@@ -109,7 +110,7 @@ public final class PooledWorkers<STATE, ITEM> extends BaseShareableLockable {
 		public void run() {
 			final STATE state;
 			try {
-				state = this.logic.initialize();
+				state = this.logic.initialize(PooledWorkers.this);
 			} catch (Throwable t) {
 				PooledWorkers.this.startupLatch.countDown();
 				workerThreadExited("Failed to initialize the worker state", null, castException(t));
--- src/main/java/com/armedia/commons/utilities/PooledWorkersLogic.java
+++ src/main/java/com/armedia/commons/utilities/PooledWorkersLogic.java
@@ -5,21 +5,21 @@
  * Copyright (C) 2013 - 2022 Armedia, LLC
  * %%
  * This file is part of the Caliente software.
- * 
+ *
  * If the software was purchased under a paid Caliente license, the terms of
  * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * Caliente is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Caliente is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Caliente. If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -34,7 +34,7 @@ import com.armedia.commons.utilities.function.CheckedTools;
 @FunctionalInterface
 public interface PooledWorkersLogic<STATE, ITEM, EX extends Throwable> {
 
-	public default STATE initialize() throws EX {
+	public default STATE initialize(PooledWorkers<STATE, ITEM> worker) throws EX {
 		return null;
 	}
 
