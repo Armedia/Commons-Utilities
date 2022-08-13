8ab10a955dcd9483c11b7e15649d575e2817ad2a
Diego Rivera <diego.rivera@armedia.com>
2022-08-13 09:14:55 -0600

    Minor fixes to remove warnings.

--- src/main/java/com/armedia/commons/utilities/cli/launcher/log/BasicLogger.java
+++ src/main/java/com/armedia/commons/utilities/cli/launcher/log/BasicLogger.java
@@ -5,21 +5,21 @@
  * Copyright (C) 2013 - 2022 Armedia, LLC
  * %%
  * This file is part of the Caliente software.
- * 
+ *
  * If the software was purchased under a paid Caliente license, the terms of
  * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * Caliente is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Caliente is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Caliente. If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -30,14 +30,13 @@ import java.io.PrintStream;
 import java.util.Date;
 
 import org.apache.commons.lang3.time.DateFormatUtils;
-import org.slf4j.Logger;
 import org.slf4j.helpers.FormattingTuple;
 import org.slf4j.helpers.MarkerIgnoringBase;
 import org.slf4j.helpers.MessageFormatter;
 
 import com.armedia.commons.utilities.Tools;
 
-public class BasicLogger extends MarkerIgnoringBase implements Logger {
+public class BasicLogger extends MarkerIgnoringBase {
 	private static final long serialVersionUID = 1L;
 
 	private static enum Level {
