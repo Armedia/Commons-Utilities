80c8a0d143969e6c97d34435a3c4f201e9c27737
diego <diego.rivera@armedia.com>
2025-05-09 09:48:06 -0600

    Downgrade from "extends Throwable" to "extends Exception"

--- src/main/java/com/armedia/commons/utilities/concurrent/TransformingFuture.java
+++ src/main/java/com/armedia/commons/utilities/concurrent/TransformingFuture.java
@@ -5,21 +5,21 @@
  * Copyright (C) 2013 - 2025 Armedia, LLC
  * %%
  * This file is part of the Caliente software.
- * 
+ *
  * If the software was purchased under a paid Caliente license, the terms of
  * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
- * 
+ *
  * Caliente is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Caliente is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Caliente. If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -55,14 +55,14 @@ import com.armedia.commons.utilities.function.CheckedTools;
 public class TransformingFuture<F, T> implements Future<T> {
 
 	private final Future<F> future;
-	private final CheckedFunction<F, T, ? extends Throwable> transformer;
+	private final CheckedFunction<F, T, ? extends Exception> transformer;
 
 	public TransformingFuture(Future<F> future, Function<F, T> transformer) {
 		this(future, CheckedTools.check(transformer));
 	}
 
 	public <E extends Exception> TransformingFuture(Future<F> future,
-		CheckedFunction<F, T, ? extends Throwable> transformer) {
+		CheckedFunction<F, T, ? extends Exception> transformer) {
 		this.future = Objects.requireNonNull(future, "Must provide a future to transform with");
 		this.transformer = Objects.requireNonNull(transformer, "Must provide a transformer function");
 	}
